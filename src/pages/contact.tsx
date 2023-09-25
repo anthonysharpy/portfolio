5c0c4ee00013ec146b179d38df97709e91e45564
Anthony Sharp <anthonysharp15@gmail.com>
2023-09-25 13:35:08 +0100

    finish contact page. tweak about page.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,15 @@
+import { Icon } from '@iconify/react';
+import './contact.css'
+
 export function ContactPage() {
     return (
         <>
+            <div className="contact-row">
+                <Icon icon="gg:phone" /><p>{" +44 7734083797"}</p>
+            </div>
+            <div className="contact-row">
+                <Icon icon="ic:round-email" /><p>{" anthonysharp15@gmail.com"}</p>
+            </div>
         </>
     )
 }
