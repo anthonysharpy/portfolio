dcb5a015f385f1cc71d84ad872790ce52c5f0783
Anthony Sharp <anthonysharp15@gmail.com>
2023-09-25 17:00:06 +0100

    improve contact page and home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 export function HomePage() {
     return (
-        <>
-            <p>I often get told that I write too much. I am currently in the process of writing a three-part novel on why this isn't true, but until then, I've written a case study on each piece of tech that I'm confident with, which you can find above (sorry, I'm still building this site as of 25/9/2023 😁).</p>
-        </>
+        <div className="centered">
+            <p>I often get told that I write too much. I am currently in the process of writing a three-part novel on why this isn't true, but until then, I've written a case study on each piece of tech that I'm confident with, which you can find above 😁</p>
+        </div>
     )
 }
