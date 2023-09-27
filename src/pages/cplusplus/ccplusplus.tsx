02c95853fe0bd0988a360b5ed0b39ce36629f91c
Anthony Sharp <anthonysharp15@gmail.com>
2023-09-27 17:25:57 +0100

    add more stuff to c++ page

--- src/pages/cplusplus/ccplusplus.tsx
+++ src/pages/cplusplus/ccplusplus.tsx
@@ -1,15 +1,18 @@
 export function CCPlusPlusPage() {
     return (
         <>
-        <p>I think I was probably 12 or 13 when I first started getting into C++. Well, I thought I was writing C++, but I didn't really get the idea of OOP at the time, so really my ugly spaghetti code was just glorified C.</p>
-        <p>That being said C++ is not something I have used commercially or something I try and keep up-to-date with. But I am familiar with it.</p>
+        <p>C++ was probably the first language I got into properly. Well, I say C++, but actually at the time my monstrous spaghetti code was more like glorified C, since I didn't really <em>get</em> what OOP was all about at the time.</p>
+        <p>That being said C++ is not something I have used commercially or something I try and keep up-to-date with. But I am familiar with it and I know that some companies do use it for part of their tech stack, so I'm putting some stuff here.</p>
         <h2>GNU Compiler Collection contributions</h2>
-        <p>add code...</p>
+        <p>A few years ago I contributed some bug fixes for the GNU C++ compiler. I won't blabber on about what they were but you can see some examples below of the kind of work I did.</p>
+        <p><a  href="https://github.com/gcc-mirror/gcc/commit/7e0f147a29f42d6149585573650bd4827f3b2b93" target="_blank">https://github.com/gcc-mirror/gcc/commit/7e0f147a29f42d6149585573650bd4827f3b2b93</a></p>
+        <p><a  href="https://github.com/gcc-mirror/gcc/commit/be246ac2d26e1cb072f205bf97d5eac150220f3f" target="_blank">https://github.com/gcc-mirror/gcc/commit/be246ac2d26e1cb072f205bf97d5eac150220f3f</a></p>
+        <p>There was also another bug which I co-authored with Jason Merrill, but I can't seem to find the commit for it (perhaps they haven't merged it in yet). But the change log entry is <a href="https://github.com/gcc-mirror/gcc/blob/36eec7995b4d53083c3ee7824bd765b5eba8b1a1/gcc/cp/ChangeLog-2021" target="_blank">here</a>.</p>
         <h2>Minecraft convolutional neural network bot</h2>
+        <p>One super-fun project I did during COVID was to create a bot that used a convolutional feed-forward neural network to learn to play Minecraft... sort of. </p>
         <img src="images/minecraft_bot_1.png"></img>
         <h2>Talisman Online bot</h2>
         <img src="images/talisman_online_bot_1.png"></img>
         </>
-
     )
 }
