418d71a1569edab9bdeb9f2a53b4e86ce05b4815
Anthony Sharp <anthonysharp15@gmail.com>
2023-10-01 19:28:06 +0100

    add blazor site demo

--- src/pages/csharp/csharp.tsx
+++ src/pages/csharp/csharp.tsx
@@ -4,6 +4,9 @@ export function CSharpPage() {
             <p>If you want to jump straight to an example of the sort of C# code I can write, see here: <a href="https://github.com/anthonysharpy/battleships" target="_blank">https://github.com/anthonysharpy/battleships</a></p>
             <p>Please see my <a href="/files/Anthony Sharp - CV September 2023.pdf" target="_blank">CV</a> for a quick run-down of the kinds of commercial experience I have with C#.</p>
             <p>I started using C# back in 2014. Unity is what got me into it. The rest of this is just going to be me talking about some of the projects I've done in C# 😁</p>
+            <h2>Blazor Server dashbaord site</h2>
+            <p>Below you can see a demonstration of a Blazor Dashboard server I put together in a weekend. You can find the code for it here: <a href="https://github.com/anthonysharpy/blazor-dashboard" target="_blank">https://github.com/anthonysharpy/blazor-dashboard</a>.</p>
+            <video controls><source src="videos/blazor_site_1.mp4"/></video>
             <h2>S&box City</h2>
             <p>S&box City is a gamemode I've been working on for over a year now developed in the s&box game engine. The entire thing is pretty much coded from scratch, including the players, vehicles, NPCs, weapons, entities, persistence, etcetera.</p>
             <p><i>"Is this really something that shows off your C# skills?"</i>. I mean - sure - it's not <i>"This is my example of a basic REST API coded in C# using a tutorial that I found on the internet"</i>, like you might see in some places. But I would say the answer is definitely yes! Here's why:</p>
