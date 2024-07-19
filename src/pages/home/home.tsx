facbdf88fc68bc4381a20ff29a1213bf19923e7a
Anthony Sharp <anthonysharp15@gmail.com>
2024-07-19 16:39:02 +0100

    update home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 export function HomePage() {
     return (
         <div className="centered">
+            <b>Note: I haven't updated this portfolio in a while, but I plan to soon (19/7/2024). In particular I want to write about some of my more recent experience, and also improve the styling of the site a bit. In the meantime, feel free to look around!</b>
             <p>I often get told that I write too much. I am currently in the process of writing a three-part novel on why this isn't true, but until then, I've written a bit about some of tech that I'm confident with, which you can find above 😁 I also have commercial experience with:</p>
             <li>Docker</li>
             <li>PostgreSQL, MongoDB, Microsoft SQL Server</li>
