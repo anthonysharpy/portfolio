6b79f93aaaf600f634926b9eb1d2fe0eff9f15f3
Anthony Sharp <anthonysharp15@gmail.com>
2024-08-03 00:11:42 +0100

    remove cv link

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,7 +4,6 @@ export function HomePage() {
             <h2>A bit about me</h2>
             <p>My name is Anthony and I am a 26 year-old software developer currently based in the United Kingdom.</p>
             <p>I have over three and a half years of commercial experience. I seem to have created a niche for myself in the financial services sector, but am happy working in any industry.</p>
-            <p>If you'd like to know more, <a href="/files/Anthony Sharp - CV November 2023.pdf" rel="noreferrer" target="_blank">my CV is here</a> and <a href="https://github.com/anthonysharpy" rel="noreferrer" target="_blank">my GitHub is here</a>.</p>
             <p>My dad was a programmer/electronics engineer in the 80s/90s and got me into programming at a young age. I started with Python at age 10 writing some really basic scripts. At age 12 I made my first tacky HTML-only website for a school project about India. From the age of about 13 I started getting into C++. At age 14 I started making things in Unity with C#. At age 17/18 I started getting more into web development, making websites, creating APIs in PHP and Go, and creating a law app for Android in Java. I guess you could say I really was a <em>junior</em> developer!</p>
         </div>
     )
