8830a430477379af0f6a422186527339d4b8da00
Anthony Sharp <anthonysharp15@gmail.com>
2024-08-03 00:10:46 +0100

    delete remnants of about page

--- src/pages/pagecontroller.ts
+++ src/pages/pagecontroller.ts
@@ -24,7 +24,7 @@ export const Routes = [
     new Page("go", "Go", GoPage(), new Color("rgb(106, 223, 108)"), new Color('white'), new Color('white'), new Color('#8c6b37')),
     new Page("csharp", "C#", CSharpPage(), new Color("rgb(149, 135, 235)"), new Color('white'), new Color('white'), new Color('#2b3a69')),
     new Page("infrastructure", "Infrastructure and Hosting", InfrastructurePage(), new Color("rgb(234, 184, 241)"), new Color('white'), new Color('white'), new Color('#ffffff')),
-    new Page("react", "React/Angular", ReactPage(), new Color("rgb(131, 171, 185)"), new Color('white'), new Color('white'), new Color('#4d7057')),
+    new Page("react", "React/Angular/Vue", ReactPage(), new Color("rgb(131, 171, 185)"), new Color('white'), new Color('white'), new Color('#4d7057')),
     new Page("ccplusplus", "C/C++", CCPlusPlusPage(), new Color("rgb(241, 132, 151)"), new Color('white'), new Color('white'), new Color('#745691')),
     //new Page("workexperience", "Work Experience", WorkExperiencePage(), new Color("rgb(239, 177, 86)"), new Color('white'), new Color('white'), new Color('#eb8d6e')),
     new Page("contact", "Contact", ContactPage(), new Color("rgb(219, 117, 233)"), new Color('white'), new Color('white'), new Color('#986ee0')),
